e5ff05fa8edbe31cfbc7283859a690bd314dcd28
salomon1102 <acaru.211@gmail.com>
2020-12-04 01:22:35 -0600

    Feat: Add ActionsGET,POST,DELETE for comentary and tags
    En esta parte se realizo las acciones para comentarios y tags, donde ya
    es posible agregar nuevos comentarios

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from publicaciones.models import  Publicacion
 from publicaciones.serializers import PublicacionSerializer
+from comentarios.serializers import ComentarioSerializer
+from tags.serializers import TagSerializer
+from comentarios.models import  Comentario
+
 from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework import viewsets
@@ -10,6 +14,43 @@ from rest_framework.decorators import action
 # Create your views here
 # .
 class PublicacionViewSet(viewsets.ModelViewSet):
+
    queryset = Publicacion.objects.all()
    serializer_class = PublicacionSerializer
    permission_classes = (AllowAny, )
+
+   @action(methods=['GET', 'POST', 'DELETE'], detail=True)
+   def tags(self, request, pk=None):
+      publicacion = self.get_object()
+
+      if request.method == 'GET':
+         serializer = TagSerializer(publicacion.tag, many=True)
+         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+   @action(methods=['GET', 'POST', 'DELETE'], detail=True)
+   def comentarios(self, request, pk=None):
+      publicacion = self.get_object()
+
+      if request.method == 'GET':
+         serializer = ComentarioSerializer(publicacion.comentarios, many=True)
+         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+      if request.method == 'POST':
+         comet_id = request.data['comentarios_ids']
+
+         for id_com in comet_id:
+            comentario = Comentario.objects.get(id=id_com)
+            publicacion.comentarios.add(comentario)
+
+         serializer = ComentarioSerializer(publicacion.comentarios, many=True)
+         return Response(status=status.HTTP_200_OK, data=serializer.data)
+   
+      if request.method == 'DELETE':
+         comet_id = request.data['comentarios_ids']
+
+         for id_com in comet_id:
+            comentario = Comentario.objects.get(id=id_com)
+            publicacion.comentarios.remove(comentario)
+
+         serializer = ComentarioSerializer(publicacion.comentarios, many=True)
+         return Response(status=status.HTTP_200_OK, data=serializer.data)
